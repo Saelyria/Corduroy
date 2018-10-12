135713902926831dc53e3bf28ce3eca2b11fee77
Aaron Bosnjak <aaron.bosnjak707@gmail.com>
2018-10-12 11:54:01 -0700

    Move work from develop

---
+++ Routing/Routable.swift
@@ -0,0 +1,75 @@
+/*
+ import Foundation
+ 
+ public typealias RoutableCoordinator = Coordinator & Routable
+ public typealias RoutableFlowCoordinator = FlowCoordinator & Routable
+ public typealias RoutableSelfCoordinating = SelfCoordinating & Routable
+ 
+ /**
+ A protocol describing a coordinator or flow coordinator that can be routed to via URLs.
+ 
+ In a URL-routed application, coordinators correspond to the path segments in a URL's path (i.e. "home" and "profiles"
+ in "/home/profiles" each correspond to a different coordinator in the navigation stack).
+ */
+ public protocol Routable: AnyCoordinator {
+ static var pathSegment: String { get }
+ }
+ 
+ 
+ 
+ public protocol RouteParameterInitable {
+ init?(routeParameters: [String: String]?)
+ }
+ 
+ 
+ //public struct Route {
+ //    static let any: Route = Route(from: nil, to: nil)
+ //
+ //    let from: Routable.Type?
+ //    let to: Routable.Type?
+ //}
+ 
+ 
+ /**
+ A protocol describing an object that can present other coordinators when asked to by a routing navigator.
+ 
+ The relationship between a `Routing` object and a `RoutingNavigator` is fairly complex. When a routing navigator is
+ asked to handle a routing URL, it will use a number of `Routing` objects in order to properly present all coordinators
+ involved in the route. Which `Routing` object it uses is a cascading list it will work its way down. These objects act
+ as delegates to the navigator and are effectively used to call `go(to:)` back on the navigator with the appropriate
+ present method/navigation parameters/setup model when their `route(to:navigator:parameters:)` method is called.
+ 
+ First, the navigator will check if the coordinator that will present the next coordinator conforms to `Routing`. If it
+ does and the coordinator to be presented is included in its `routableCoordinators` array, it will delegate the
+ presentation to the coordinator. This is the recommended way to handle the presentation of coordinators that have
+ dependencies, especially dependencies like 'delegate' objects that must be passed to them from other specific
+ coordinators.
+ 
+ If the top coordinator doesn't conform to `Routing` or the coordinator being presented is not in its list of routable
+ coordinators, the navigator will fall back to the objects in its `routingDelegates` array, checking for the first
+ object in the array that has the given coordinator in its `routableCoordinators` array.
+ 
+ If none of the navigator's routing delegates can handle the coordinator route, it will attempt to handle the
+ navigation itself.
+ */
+ public protocol Routing: AnyObject {
+ /**
+ The list of coordinators that this coordinator can route to.
+ 
+ A routing navigator will only ever ask a coordinator to route to another coordinator if the latter coordinator's
+ type is included in this array. An empty array will be taken to mean this coordinator can route to any other
+ coordinator.
+ */
+ var routableCoordinators: [Routable.Type] { get }
+ 
+ /**
+ Asks the routing object to route to the coordinator with the given path segment.
+ 
+ A routing navigator will call this method on a routable coordinator
+ - parameter pathSegment: The path segment
+ - parameter parameters: The key-value parameters that were included with the path segment for the
+ presented coordinator in the URL being handled by the routing navigator.
+ */
+ func route(toCoordinatorFor pathSegment: String, navigator: RoutingNavigator, parameters: [String: String]?)
+ }
+ */
---
+++ Routing/RoutingURLParser.swift
@@ -0,0 +1,63 @@
+/*
+ import Foundation
+ 
+ /**
+ A protocol describing an object that parses a URL into individual path segments and parameters for a routing navigator.
+ 
+ The primary job of a routing URL parser is to, from a given URL (most likely from a universal link), return the
+ individual path segments and their respective parameters (from either the URL's path parameters or its query items).
+ For example, a URL parser would take this URL:
+ ```
+ '.../home/profiles/profile?name=john'
+ ```
+ then would likely return something like the following array:
+ ```
+ [
+ (pathSegment: "home", parameters: [:]),
+ (pathSegment: "profiles", parameters: [:]),
+ (pathSegment: "profile", parameters: ["name":"john"])
+ ]
+ ```
+ This object can also be used to better translate universal link URLs into routing URLs, especially where intermediary
+ path segments are implied in the universal link URL. For example, a parser could be given '.../profile?name=john'
+ as a universal link to the app and wish to extend it to '.../home/profiles/profile?name=john' so that the coordinator
+ stack created by the router is more complete. A custom URL parser is also used for filtering or mapping the given
+ query items to the appropriate path segments.
+ 
+ Its only method is `pathSegmentsAndParameters(from:)`, where it is expected to,
+ with the given URL, return an array of the path segments (each corresponding to a coordinator) and the parameters for
+ each segment.
+ */
+ protocol RoutingURLParser {
+ typealias PathSegment = (pathSegment: String, parameters: [String: String])
+ 
+ var parseableUrls: [URL] { get }
+ 
+ func pathSegments(from url: URL) -> [PathSegment]
+ }
+ 
+ 
+ 
+ /**
+ A default routing URL parser that returns all the path segments in the given URL and gives all query items from to URL
+ to each path segment as its 'route parameters'.
+ */
+ class DefaultRoutingURLParser: RoutingURLParser {
+ let parseableUrls: [URL] = []
+ 
+ func pathSegments(from url: URL) -> [RoutingURLParser.PathSegment] {
+ let segments = url.pathComponents.filter({ $0 != "/" })
+ let urlComponents = URLComponents(string: url.absoluteString)!
+ var parameters: [String: String] = [:]
+ urlComponents.queryItems?.forEach({ (item) in
+ parameters[item.name] = item.value ?? ""
+ })
+ 
+ var pathSegmentsAndParameters: [RoutingURLParser.PathSegment] = []
+ for segment in segments {
+ pathSegmentsAndParameters.append((pathSegment: segment, parameters: parameters))
+ }
+ return pathSegmentsAndParameters
+ }
+ }
+ */
